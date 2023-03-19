84af22ab62586ab8f95dcda17b0c73127f04b863
Magino Marveaux-Cochet <magino.cochet@gmail.com>
2023-03-19 11:19:19 +0100

    docs: align jsdoc

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,13 @@ type BitcoinMonthProjection = {
  * - usdAccumulated: The average value of bitcoin accumulated by month in USD.
  * - the new bitcoin dollar price.
  * It assumes that the bitcoin value will increase by 2% every month.
- * @param {number} amountInvestedByMonth - The amount invested by month in USD
- * @param {number} numberOfYears - The number of years to project
- * @param {number} bitcoinPriceInUSD - The bitcoin price in USD
- * @param {number} percentIncreasePerMonth - The percent increase per month of bitcoin in USD
- * @returns {object} - The array of each month's average value of bitcoin accumulated in Bitcoin and USD.
+ * @param @type {number} amountInvestedByMonthInUSD - The amount invested by month in USD
+ * @param @type {number} numberOfYears - The number of years to project
+ * @param @type {number} startingBitcoinPriceInUSD - The bitcoin price in USD
+ * @param @type {number} btcPercentIncreasePerMonthInUSD - The percent increase per month of bitcoin in USD
+ * @returns @type {BitcoinMonthProjection[]} - An array of objects with the month's bitcoin value in Bitcoin and USD.
  */
-const dcaProjection = (amountInvestedByMonth = 100, numberOfYears = 4, startingBitcoinPriceInUSD = 20000, btcPercentIncreasePerMonthInUSD = 2): BitcoinMonthProjection[] => {
+const dcaProjection = (amountInvestedByMonthInUSD = 100, numberOfYears = 4, startingBitcoinPriceInUSD = 20000, btcPercentIncreasePerMonthInUSD = 2): BitcoinMonthProjection[] => {
     const bitcoinProjection = [];
     const accumulator = {
       bitcoinAccumulated: 0,
@@ -29,9 +29,9 @@ const dcaProjection = (amountInvestedByMonth = 100, numberOfYears = 4, startingB
     }
     for (let month = 1; month <= numberOfYears * 12; month++) {
       const bitcoinPriceForMonth = accumulator.bitcoinPriceInUSD * (1 + btcPercentIncreasePerMonthInUSD / 100);
-      const usdInvested = amountInvestedByMonth * month;
+      const usdInvested = amountInvestedByMonthInUSD * month;
       const usdAccumulated = accumulator.bitcoinAccumulated * bitcoinPriceForMonth;
-      const bitcoinAccumulated = accumulator.bitcoinAccumulated + amountInvestedByMonth / bitcoinPriceForMonth;
+      const bitcoinAccumulated = accumulator.bitcoinAccumulated + amountInvestedByMonthInUSD / bitcoinPriceForMonth;
   
       bitcoinProjection.push({
         month,
