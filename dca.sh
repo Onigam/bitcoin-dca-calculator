e7a72c9e0d3864df661edf0b31fbfb3b4a4ac817
Magino Marveaux-Cochet <magino.cochet@gmail.com>
2023-03-19 11:09:17 +0100

    fix(script): set correct import call

--- dca.sh
+++ dca.sh
@@ -27,9 +27,10 @@ read -p "Number of years (by default 4 years):" numberOfYears
 read -p "Starting bitcoin price in USD (by default 20000 USD): " startingBitcoinPriceInUSD
 read -p "Percentage of increase by month of the Bitcoin price in USD (Hypothesis by default 2%): " btcPercentIncreasePerMonthInUSD
 
+amountInvestedByMonth=${amountInvestedByMonth:-100}
 numberOfYears=${numberOfYears:-4}
 startingBitcoinPriceInUSD=${startingBitcoinPriceInUSD:-20000}
 btcPercentIncreasePerMonthInUSD=${btcPercentIncreasePerMonthInUSD:-2}
 
 # Execute the JavaScript function with the provided parameters
-node -e "const projection = require('./index.js'); console.table(projection(${amountInvestedByMonth}, ${numberOfYears}, ${startingBitcoinPriceInUSD}, ${btcPercentIncreasePerMonthInUSD}));"
+node -e "const projection = require('./dist/index.js'); console.table(projection.default(${amountInvestedByMonth}, ${numberOfYears}, ${startingBitcoinPriceInUSD}, ${btcPercentIncreasePerMonthInUSD}));"
